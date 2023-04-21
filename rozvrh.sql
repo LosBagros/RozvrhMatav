12e3e65f832c2b18851c4587fef22ea290812b32
Bagros <wifi5@centrum.cz>
2023-04-21 23:22:14 +0200

    updated db

--- rozvrh.sql
+++ rozvrh.sql
@@ -2,29 +2,70 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
 
 CREATE TABLE `classes` (
   `class_id` int(11) NOT NULL,
-  `nazev` varchar(32) NOT NULL
+  `classname` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `classrooms` (
   `classroom_id` int(11) NOT NULL,
-  `nazev` varchar(32) NOT NULL
+  `name` varchar(16) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `days` (
-  `day_id` int(11) NOT NULL
+  `day_id` int(11) NOT NULL,
+  `name` varchar(8) NOT NULL,
+  `shortcut` varchar(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `hours` (
-  `hour_id` int(11) NOT NULL
+  `hour_id` int(11) NOT NULL,
+  `start` time NOT NULL,
+  `end` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `login` (
+  `id` int(11) NOT NULL,
+  `email` varchar(32) NOT NULL,
+  `pass` varchar(64) NOT NULL,
+  `admin` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `login` (`id`, `email`, `pass`, `admin`) VALUES
+(7, 'admin@bagros.eu', '$2y$10$dPiBDc2S25gXPrjJwn0gsunJbpOBYgRiBAfJ31knZtcvbOOQEyVK.', 1),
+(8, 'admin1@bagros.eu', '$2y$10$8Typ3oKmbyhrmHKWIqXacuKvpjmLyUSZwK0RTbRiBYXZnT0Fr65D6', 0),
+(9, 'test@test.cz', '$2y$10$NIqF1v.H4VWl.OEygvAdxOQ8ps0nDR2.WMOG0zZBwgcIyMf0FQFbm', 0),
+(10, 'tom@bagros.eu', '$2y$10$c3J277f7k/qymG.819AY5.4pofssn9Tsp/09T35PM6TDf7QUqygU.', 0);
+
+CREATE TABLE `students` (
+  `student_id` int(11) DEFAULT NULL,
+  `login_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `students` (`student_id`, `login_id`) VALUES
+(NULL, NULL),
+(NULL, NULL),
+(NULL, NULL),
+(NULL, NULL);
 
 CREATE TABLE `subjects` (
   `subject_id` int(11) NOT NULL,
-  `nazev` varchar(32) NOT NULL,
-  `zkratka` varchar(8) NOT NULL
+  `name` varchar(32) NOT NULL,
+  `shortcut` varchar(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `teachers` (
+  `class_id` int(11) DEFAULT NULL,
+  `teachear_id` int(11) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  `surname` varchar(32) DEFAULT NULL,
+  `username` varchar(8) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `timetable` (
@@ -35,21 +76,6 @@ CREATE TABLE `timetable` (
   `subject_id` int(11) DEFAULT NULL,
   `classroom_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `email` varchar(32) NOT NULL,
-  `pass` varchar(64) NOT NULL,
-  `admin` tinyint(1) NOT NULL,
-  `class_id` int(11) DEFAULT NULL,
-  `role` enum('student','teacher') NOT NULL DEFAULT 'student'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `users` (`id`, `email`, `pass`, `admin`, `class_id`, `role`) VALUES
-(7, 'admin@bagros.eu', '$2y$10$dPiBDc2S25gXPrjJwn0gsunJbpOBYgRiBAfJ31knZtcvbOOQEyVK.', 1, NULL, 'student'),
-(8, 'admin1@bagros.eu', '$2y$10$8Typ3oKmbyhrmHKWIqXacuKvpjmLyUSZwK0RTbRiBYXZnT0Fr65D6', 0, NULL, 'student'),
-(9, 'test@test.cz', '$2y$10$NIqF1v.H4VWl.OEygvAdxOQ8ps0nDR2.WMOG0zZBwgcIyMf0FQFbm', 0, NULL, 'student'),
-(10, 'tom@bagros.eu', '$2y$10$c3J277f7k/qymG.819AY5.4pofssn9Tsp/09T35PM6TDf7QUqygU.', 0, NULL, 'student');
 
 
 ALTER TABLE `classes`
@@ -64,8 +90,19 @@ ALTER TABLE `days`
 ALTER TABLE `hours`
   ADD PRIMARY KEY (`hour_id`);
 
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `students`
+  ADD UNIQUE KEY `login_id` (`login_id`),
+  ADD KEY `students_ibfk_1` (`student_id`);
+
 ALTER TABLE `subjects`
   ADD PRIMARY KEY (`subject_id`);
+
+ALTER TABLE `teachers`
+  ADD UNIQUE KEY `login_id` (`teachear_id`),
+  ADD KEY `teachers_ibfk_1` (`class_id`);
 
 ALTER TABLE `timetable`
   ADD PRIMARY KEY (`id`),
@@ -74,11 +111,6 @@ ALTER TABLE `timetable`
   ADD KEY `timetable_hour_fk` (`hour_id`),
   ADD KEY `timetable_subject_fk` (`subject_id`),
   ADD KEY `classroom_id` (`classroom_id`);
-
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `class_id` (`class_id`);
 
 
 ALTER TABLE `classes`
@@ -99,9 +131,14 @@ ALTER TABLE `subjects`
 ALTER TABLE `timetable`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `classes` (`class_id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`login_id`) REFERENCES `login` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
+
+ALTER TABLE `teachers`
+  ADD CONSTRAINT `teachers_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`) ON DELETE SET NULL ON UPDATE NO ACTION,
+  ADD CONSTRAINT `teachers_ibfk_2` FOREIGN KEY (`teachear_id`) REFERENCES `login` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION;
 
 ALTER TABLE `timetable`
   ADD CONSTRAINT `classroom_id` FOREIGN KEY (`classroom_id`) REFERENCES `classrooms` (`classroom_id`),
@@ -109,7 +146,8 @@ ALTER TABLE `timetable`
   ADD CONSTRAINT `timetable_day_fk` FOREIGN KEY (`day_id`) REFERENCES `days` (`day_id`) ON DELETE SET NULL ON UPDATE NO ACTION,
   ADD CONSTRAINT `timetable_hour_fk` FOREIGN KEY (`hour_id`) REFERENCES `hours` (`hour_id`) ON DELETE SET NULL ON UPDATE NO ACTION,
   ADD CONSTRAINT `timetable_subject_fk` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`subject_id`) ON DELETE SET NULL ON UPDATE NO ACTION;
-
-ALTER TABLE `users`
-  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`) ON DELETE SET NULL ON UPDATE NO ACTION;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
